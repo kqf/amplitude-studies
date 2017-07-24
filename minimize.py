49337d84812510fc1a62b3e6aca884e88a5ad875
Iohan Petrov <petrov.knu@gmail.com>
2017-07-24 09:58:43 +0200

    Added plots and averages to test the performance.

--- minimize.py
+++ minimize.py
@@ -4,11 +4,27 @@
 from scipy.optimize import minimize, rosen
 import operator
 from numpy.random import rand
+import seaborn
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def iterations(x, a):
+	res = minimize(rosen, x, method=a, tol=1e-6)
+	try:
+		if res.success:
+			return res.nit
+		return 0
+	except AttributeError:
+		return 999999999
+
+def check(data, a):
+	res = np.apply_along_axis(lambda x: iterations(x, a), 0, data)
+	return res.mean(), np.std(res)
 
 
-def scan(n = 4):
-	x0 = rand(n)
-	# x0 = [1.3, 0.7, 0.8, 1.9, 1.2]
+def scan(n = 4, ntrials = 10):
+	data = rand(n, ntrials)
 	# Need jacobian:
 	# , 'Newton-CG'
 	# , 'dogleg'
@@ -16,22 +32,28 @@ def scan(n = 4):
 	# , 'COBYLA'
 	algorithms = 'Nelder-Mead', 'Powell', 'CG', 'BFGS', 'L-BFGS-B', 'TNC', 'SLSQP'
 
+	results = [check(data, a) for a in algorithms]
+	mean_iterations = {a: i for a, (i, e) in zip(algorithms, results) if i > 0.0}
 
-	f = lambda x: minimize(rosen, x0, method=x, tol=1e-6)
-	data = {a: f(a) for a in algorithms}
-	iterations = {a: r.nit for a, r in data.iteritems()}
-
-	for a, niter in iterations.iteritems():
-		string = a, niter, len(x0)
+	for a, niter in mean_iterations.iteritems():
+		string = a, niter, n 
 		print "The algorithm {0} requires {1} iterations for {2} parameters".format(*string)
 
 	print 'The best algorithm is'
-	print min(iterations.iteritems(), key=operator.itemgetter(1))
+	print min(mean_iterations.iteritems(), key=operator.itemgetter(1))
+
+	y, err = zip(*results)
+	x = np.ogrid[0:len(y)]
+
+	plt.errorbar(x, y, yerr=err)
+	plt.xticks(x, mean_iterations.keys(), rotation='vertical')
+	plt.yscale("log", nonposx='clip')
+	plt.show()
 
 
 def main():
-	for i in range(10, 40):
-		scan(i)
+	# for i in range(10, 40): scan(i)
+	scan(40)
 
 
 if __name__ == '__main__':
