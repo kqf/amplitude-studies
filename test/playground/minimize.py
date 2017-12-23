48ad2a9166730e62e467746534660beae2678e61
Sasha <q.dev.null@gmail.com>
2017-12-23 22:54:17 +0100

    Refactor playground tests

--- test/playground/minimize.py
+++ test/playground/minimize.py
@@ -1,62 +1,64 @@
-#!/usr/bin/python
-
-
 from scipy.optimize import minimize, rosen
 import operator
 from numpy.random import rand
-import seaborn
+import seaborn  # noqa
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 from minuitmethod import MinuitMethod
 
+
 def iterations(x, a):
-	res = minimize(rosen, x, method=a, tol=1e-6)
-	try:
-		if res.success:
-			return res.nit
-		return 0
-	except AttributeError:
-		return 999999999
+    res = minimize(rosen, x, method=a, tol=1e-6)
+    try:
+        if res.success:
+            return res.nit
+        return 0
+    except AttributeError:
+        return 999999999
+
 
 def check(data, a):
-	res = np.apply_along_axis(lambda x: iterations(x, a), 0, data)
-	return res.mean(), np.std(res)
+    res = np.apply_along_axis(lambda x: iterations(x, a), 0, data)
+    return res.mean(), np.std(res)
 
 
-def scan(n = 4, ntrials = 10):
-	data = rand(n, ntrials)
-	# Need jacobian:
-	# , 'Newton-CG'
-	# , 'dogleg'
-	# , 'trust-ncg'	
-	# , 'COBYLA'
-	algorithms = 'Nelder-Mead', 'Powell', 'CG', 'BFGS', 'L-BFGS-B', 'TNC', 'SLSQP', MinuitMethod()
+def scan(n=4, ntrials=10):
+    data = rand(n, ntrials)
+    # Need jacobian:
+    # , 'Newton-CG'
+    # , 'dogleg'
+    # , 'trust-ncg'
+    # , 'COBYLA'
+    algorithms = ('Nelder-Mead', 'Powell', 'CG', 'BFGS',
+                  'L-BFGS-B', 'TNC', 'SLSQP', MinuitMethod())
 
-	results = [check(data, a) for a in algorithms]
-	mean_iterations = {a: i for a, (i, e) in zip(algorithms, results) if i > 0.0}
+    results = [check(data, a) for a in algorithms]
+    mean_iterations = {a: i for a, (i, e) in zip(
+        algorithms, results) if i > 0.0}
 
-	for a, niter in mean_iterations.iteritems():
-		string = a, niter, n 
-		print "The algorithm {0} requires {1} iterations for {2} parameters".format(*string)
+    for a, niter in mean_iterations.iteritems():
+        string = a, niter, n
+        msg = "The algorithm {0} requires {1} iterations for {2} parameters" 
+        print msg.format(*string)
 
-	print 'The best algorithm is'
-	print min(mean_iterations.iteritems(), key=operator.itemgetter(1))
+    print 'The best algorithm is'
+    print min(mean_iterations.iteritems(), key=operator.itemgetter(1))
 
-	y, err = zip(*results)
-	x = np.ogrid[0:len(y)]
+    y, err = zip(*results)
+    x = np.ogrid[0:len(y)]
 
-	plt.errorbar(x, y, yerr=err)
-	plt.xticks(x, mean_iterations.keys(), rotation='vertical')
-	plt.yscale("log", nonposx='clip')
-	plt.show()
+    plt.errorbar(x, y, yerr=err)
+    plt.xticks(x, mean_iterations.keys(), rotation='vertical')
+    plt.yscale("log", nonposx='clip')
+    plt.show()
 
 
 def main():
-	# for i in range(10, 40): scan(i)
-	scan(40)
+    # for i in range(10, 40): scan(i)
+    scan(40)
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- test/playground/minuitmethod.py
+++ test/playground/minuitmethod.py
@@ -8,70 +8,70 @@ from array import array
 
 class MinuitMethod(object):
 
-	def __init__(self, xtol = 1e-6):
-		super(MinuitMethod, self).__init__()
-		self.xtol = xtol
-		self.nit = 0
+    def __init__(self, xtol=1e-6):
+        super(MinuitMethod, self).__init__()
+        self.xtol = xtol
+        self.nit = 0
 
+    def fcn(self, npar, gin, f, par, iflag):
+        # This is a nuissance of ROOT interface
+        self.nit += 1
+        pars = [par[i] for i in range(self.npars)]
+        return self.func(pars)
 
-	def fcn(self, npar, gin, f, par, iflag):
-		# This is a nuissance of ROOT interface
-		self.nit += 1
-		pars = [par[i] for i in range(self.npars)]
-		return self.func(pars)
+    def __call__(self, fun, x0, args, **kwargs):
+        self.func = fun
+        self.npars = len(x0)
+        minuit = ROOT.TMinuit(self.npars)
+        minuit.SetFCN(self.fcn)
+        ierflg = ROOT.Long(2999)
+        for i, p in enumerate(x0):
+            minuit.mnparm(i, str(p), p, self.xtol, 0, 0, ierflg)
 
+        # Setup the minuit parameters
+        arglist = array('d', 10 * [0.])
 
-	def __call__(self, fun, x0, args, **kwargs):
-		self.func = fun
-		self.npars = len(x0)
-		minuit = ROOT.TMinuit(self.npars)
-		minuit.SetFCN(self.fcn)
-		ierflg = ROOT.Long(2999)
-		for i, p in enumerate(x0):
-			minuit.mnparm(i, str(p), p, self.xtol, 0, 0, ierflg)
+        arglist[0] = 1
+        minuit.mnexcm("SET ERR", arglist, 1, ierflg)
 
-		# Setup the minuit parameters
-		arglist = array('d', 10*[0.])
+        arglist[0] = 500
+        arglist[1] = 1.
+        self.nit = 0
+        minuit.mnexcm("MIGRAD", arglist, 2, ierflg)
+        amin, edm, errdef = ROOT.Double(
+            0.18), ROOT.Double(0.19), ROOT.Double(0.20)
+        nvpar, nparx, icstat = ROOT.Long(
+            1983), ROOT.Long(1984), ROOT.Long(1985)
+        minuit.mnstat(amin, edm, errdef, nvpar, nparx, icstat)
 
-		arglist[0] = 1
-		minuit.mnexcm("SET ERR", arglist, 1, ierflg)
+        success = amin < 2.0 and edm < kwargs['tol']
+        msg = 'Success' if success else 'Fail'
 
-		arglist[0] = 500
-		arglist[1] = 1.
-		self.nit = 0
-		minuit.mnexcm("MIGRAD", arglist, 2, ierflg)
-		# result = OptimizeResult(fun=fval, direc=direc, nit=iter, nfev=fcalls[0], status=warnflag, success=(warnflag == 0), message=msg, x=x)
+        optpar = [0] * self.npars
+        opterr = [0] * self.npars
 
-		amin, edm, errdef = ROOT.Double(0.18), ROOT.Double(0.19), ROOT.Double(0.20)
-		nvpar, nparx, icstat = ROOT.Long(1983), ROOT.Long(1984), ROOT.Long(1985)
-		minuit.mnstat(amin, edm, errdef, nvpar, nparx, icstat)
+        # This will not work due to pyROOT limitations
+        # for i, (x, ex) in enumerate(zip(optpar, opterr)):
 
-		success = amin < 2.0 and edm < kwargs['tol']
-		msg = 'Success' if success else 'Fail'
+        for i in range(self.npars):
+            x, ex = ROOT.Double(0), ROOT.Double(0)
+            minuit.GetParameter(i, x, ex)
+            optpar[i] = x
+            opterr[i] = ex
 
-		optpar = [0] * self.npars
-		opterr = [0] * self.npars
+        return OptimizeResult(nit=self.nit, success=success,
+                              message=msg, x=optpar)
 
-		# This will not work due to pyROOT limitations
-		# for i, (x, ex) in enumerate(zip(optpar, opterr)):
-
-		for i in range(self.npars):
-			x, ex = ROOT.Double(0), ROOT.Double(0)
-			minuit.GetParameter(i, x, ex)
-			optpar[i] = x
-			opterr[i] = ex
-
-		return OptimizeResult(nit=self.nit, success=success, message = msg, x=optpar)
-
-	def __str__(self):
-		return 'Minuit'
+    def __str__(self):
+        return 'Minuit'
 
 
 def main():
-	x = np.random.rand(4)
-	res = minimize(rosen, x, method=MinuitMethod(), tol=1e-6)
-	
-	print MinuitMethod()
+    x = np.random.rand(4)
+    res = minimize(rosen, x, method=MinuitMethod(), tol=1e-6)
+    print res
+    print MinuitMethod()
+
 
 if __name__ == '__main__':
-	main()
+    main()
